61ec0c44768530c8548dadaba2cb266d746784bc
Ivo Hulínský <i.hulinsky@gmail.com>
2021-03-22 10:18:41 +0100

    Added resource and folder data structure

--- main.tf
+++ main.tf
@@ -25,14 +25,20 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+data "vsphere_resource_pool" "pool" {
+  name          = var.vsphere_resource_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 data "vsphere_virtual_machine" "template" {
   name          = var.vm_template_name
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  name = "terraform-test"
+  # resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  resource_pool_id = data.vsphere_resource_pool.pool.id
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "vsphere_user" {
     type = string
+    sensitive = true
 }
 
 variable "vsphere_password" {
     type = string
+    sensitive = true
 }
 
 variable "vsphere_server" {
     type = string
+    sensitive = true
 }
 
 variable "vsphere_datacenter" {  
@@ -22,8 +25,15 @@ variable "vsphere_compute_cluster" {
     type = string
 }
 
+variable "vsphere_resource_pool" {
+    type = string
+}
 variable "vsphere_network" {
     type = string
+}
+
+variable "vsphere_folder" {
+  type = string
 }
 
 variable "vm_template_name" {
